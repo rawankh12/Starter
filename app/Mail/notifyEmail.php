4d6b87c812872a708214bab29a8a57d82138094e
rawan <rawan@outlook.com>
2024-03-24 01:23:01 +0200

    -me again

--- app/Mail/notifyEmail.php
+++ app/Mail/notifyEmail.php
@@ -45,7 +45,7 @@ class notifyEmail extends Mailable
     public function content()
     {
         return new Content(
-            view: 'emails.mailUsers',
+            view('emails.mailUsers')->with(['data' => $this->details])
             
         );
     }
