920ae90b803894e58b19abde3f77e552f4b1a952
rawan <rawan@outlook.com>
2024-03-24 21:41:01 +0200

    -me again

--- app/Mail/notifyEmail.php
+++ app/Mail/notifyEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NotifyEmail extends Mailable
+class notifyEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
